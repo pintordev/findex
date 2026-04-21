8fa52512424142afc26fd241c165bdbaccead523
pintor.dev <pintor.dev@gmail.com>
2026-04-21 13:03:26 +0900

    feat: 서비스 배포 주소 CORS 설정 추가

---
+++ src/main/java/com/sprint/mission/findex/global/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.sprint.mission.findex.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/api/**")
+        .allowedOrigins(
+            "https://findex-by-code-slayers.up.railway.app"
+        )
+        .allowedMethods("GET", "POST", "PATCH", "DELETE", "OPTIONS")
+        .allowedHeaders("*")
+        .allowCredentials(true)
+        .maxAge(3600);
+  }
+}
