a5d68796fbd07bc8adec37cc945c9c00ce1532a9
pintor.dev <pintor.dev@gmail.com>
2026-04-21 12:31:29 +0900

    fix: GlobalExceptionHandler 서버 로그 누락 추가

--- src/main/java/com/sprint/mission/findex/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/sprint/mission/findex/global/exception/GlobalExceptionHandler.java
@@ -4,17 +4,20 @@ import static com.sprint.mission.findex.global.exception.ApiException.ERROR.*;
 
 import com.sprint.mission.findex.global.exception.ApiException.ERROR;
 import java.util.stream.Collectors;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
   @ExceptionHandler(ApiException.class)
   public ResponseEntity<ErrorResponse> handleApiException(ApiException e) {
     ERROR error = e.getError();
+    log.warn("[ApiException] {} status: {}, code: {}, message: {}", error.name(), error.getHttpStatus().value(), error.getCode(), error.getMessage());
     return ResponseEntity
         .status(error.getHttpStatus())
         .body(ErrorResponse.of(
@@ -31,6 +34,7 @@ public class GlobalExceptionHandler {
     String details = e.getBindingResult().getFieldErrors().stream()
         .map(fe -> fe.getField() + ": " + fe.getDefaultMessage())
         .collect(Collectors.joining(", "));
+    log.warn("[ApiException] {} status: {}, code: {}, message: {} | {}", error.name(), error.getHttpStatus().value(), error.getCode(), error.getMessage(), details);
     return ResponseEntity
         .status(error.getHttpStatus())
         .body(ErrorResponse.of(
@@ -43,6 +47,7 @@ public class GlobalExceptionHandler {
   @ExceptionHandler(Exception.class)
   public ResponseEntity<ErrorResponse> handleException(Exception e) {
     ERROR error = COMMON_UNEXPECTED_ERROR;
+    log.error("[UnexpectedException] ", e);
     return ResponseEntity
         .status(error.getHttpStatus())
         .body(ErrorResponse.of(
