06d6ad50daaf1cc6ec6e9f2f77d6efb2522a4c11
pintor.dev <pintor.dev@gmail.com>
2026-04-21 22:47:44 +0900

    fix: 빌드 에러 수정을 위한 Swagger 어노테이션 임포트 추가

--- src/main/java/com/sprint/mission/findex/domain/syncjob/controller/api/SyncJobApi.java
+++ src/main/java/com/sprint/mission/findex/domain/syncjob/controller/api/SyncJobApi.java
@@ -5,6 +5,7 @@ import com.sprint.mission.findex.domain.syncjob.dto.SyncJobQueryCondition;
 import com.sprint.mission.findex.domain.syncjob.dto.SyncJobResponse;
 import com.sprint.mission.findex.global.common.dto.CursorPageResponse;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
