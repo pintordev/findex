b10dda90a5de572123d01dc8787c946051ee929d
pintor.dev <pintor.dev@gmail.com>
2026-04-22 00:52:23 +0900

    fix: 입력 Type 오류 에러 핸들링

--- src/main/java/com/sprint/mission/findex/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/sprint/mission/findex/global/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.sprint.mission.findex.global.exception;
 
-import static com.sprint.mission.findex.global.exception.ApiException.ERROR.*;
+import static com.sprint.mission.findex.global.exception.ApiException.ERROR.COMMON_INVALID_REQUEST;
+import static com.sprint.mission.findex.global.exception.ApiException.ERROR.COMMON_UNEXPECTED_ERROR;
 
 import com.sprint.mission.findex.global.exception.ApiException.ERROR;
 import java.util.stream.Collectors;
@@ -9,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 @Slf4j
 @RestControllerAdvice
@@ -17,7 +19,8 @@ public class GlobalExceptionHandler {
   @ExceptionHandler(ApiException.class)
   public ResponseEntity<ErrorResponse> handleApiException(ApiException e) {
     ERROR error = e.getError();
-    log.warn("[ApiException] {} status: {}, code: {}, message: {}", error.name(), error.getHttpStatus().value(), error.getCode(), error.getMessage());
+    log.warn("[ApiException] {} status: {}, code: {}, message: {}", error.name(),
+        error.getHttpStatus().value(), error.getCode(), error.getMessage());
     return ResponseEntity
         .status(error.getHttpStatus())
         .body(ErrorResponse.of(
@@ -34,7 +37,27 @@ public class GlobalExceptionHandler {
     String details = e.getBindingResult().getFieldErrors().stream()
         .map(fe -> fe.getField() + ": " + fe.getDefaultMessage())
         .collect(Collectors.joining(", "));
-    log.warn("[ApiException] {} status: {}, code: {}, message: {} | {}", error.name(), error.getHttpStatus().value(), error.getCode(), error.getMessage(), details);
+    log.warn("[ApiException] {} status: {}, code: {}, message: {} | {}", error.name(),
+        error.getHttpStatus().value(), error.getCode(), error.getMessage(), details);
+    return ResponseEntity
+        .status(error.getHttpStatus())
+        .body(ErrorResponse.of(
+            error.getHttpStatus().value(),
+            error.getMessage(),
+            error.getCode() + " | " + details
+        ));
+  }
+
+  @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+  public ResponseEntity<ErrorResponse> handleTypeMismatchException(
+      MethodArgumentTypeMismatchException e) {
+    ERROR error = COMMON_INVALID_REQUEST;
+    String details = String.format("%s: %s 타입이어야 합니다",
+        e.getName(),
+        e.getRequiredType() != null ? e.getRequiredType().getSimpleName() : "unknown"
+    );
+    log.warn("[ApiException] {} status: {}, code: {}, message: {} | {}", error.name(),
+        error.getHttpStatus().value(), error.getCode(), error.getMessage(), details);
     return ResponseEntity
         .status(error.getHttpStatus())
         .body(ErrorResponse.of(
@@ -47,7 +70,8 @@ public class GlobalExceptionHandler {
   @ExceptionHandler(Exception.class)
   public ResponseEntity<ErrorResponse> handleException(Exception e) {
     ERROR error = COMMON_UNEXPECTED_ERROR;
-    log.error("[UnexpectedException] ", e);
+    log.error("[UnexpectedException] cause: {}, message: {}", e.getClass().getSimpleName(),
+        e.getCause() != null ? e.getCause().getMessage() : e.getMessage());
     return ResponseEntity
         .status(error.getHttpStatus())
         .body(ErrorResponse.of(
